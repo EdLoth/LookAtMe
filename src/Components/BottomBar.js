d99bcf4cb9dd62f74c61608578dbfd9bcd6937bb
Eduardo Ramos <57899212+EdLoth@users.noreply.github.com>
2021-05-31 17:12:49 -0300

    Update LookAtMe :tada:

--- src/Components/BottomBar.js
+++ src/Components/BottomBar.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
-import styles from '../Styles/Components/Bottombar.module.css';
+import '../Styles/Components/Bottombar.css';
 
 export default function BottomBar() {
   return (
-    <div class={styles.navbar}>
-      <button to="/" class={styles.buttonBottomBar}>Home</button>
-      <button to="/About" class={styles.buttonBottomBar}>About</button>
-      <button to="/Contact" class={styles.buttonBottomBar}>Contact</button>
-      <button to="/Works" class={styles.buttonBottomBar}>Works</button>
+    <div className="NavContainer">
+      <nav className="navbar navbar-expand-lg navbar-dark gris scrolling-navbar fixed-top text-center">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="navContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" id="nameNav" href="#groupButtons">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="nameNav" href="#section2">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="nameNav" href="#section3">Works</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="nameNav" href="#team">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import styles from '../Styles/Pages/Home.module.css';
+import Typewriter from "typewriter-effect";
+
+import banner from '../assets/banner.png';
 
 export default function Home() {
-  return(
-    <h1 class={styles.titleHome}>Home Page</h1>
+  return (
+    <div id={styles.homeContentPage} className="container-fluid">
+      <div className="row">
+        <div className="col-6">
+          <div id={styles.contentPageHome} className="container">
+            <p id={styles.titleHomePage}>Olá,</p>
+            <div className={styles.displayNameHome}>
+              <p id={styles.subTitleHomePage}>Eu sou </p>
+              <p id={styles.nameHomePage}>{"<Eduardo Ramos>"}</p>
+            </div>
+            <div className={styles.displayNameHome}>
+              <p id={styles.descriptionHomePage}>
+                Desenvolvedor
+            </p>
+              <p id={styles.functionHomePage}>
+                <Typewriter
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                    delay: 50,
+                    strings: [
+                      "React JS.",
+                      "Next JS.",
+                      "React Native.",
+                      "Flutter.",
+                      "Web.",
+                    ]
+                  }}
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={banner} alt=""/>
+        </div>
+      </div>
+    </div>
   );
 }
