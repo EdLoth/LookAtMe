3b96209c41f14c3711be238097a28eb70b77a259
Eduardo Ramos <eduardosram10@gmail.com>
2021-06-01 17:35:03 -0300

    Update :tada

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import logo from '../assets/logo.jpeg';
 import '../Styles/Components/Bottombar.css';
 
 export default function NavBar() {
   return (
     <div className="NavContainer">
       <nav className="navbar navbar-expand-lg navbar-dark gris scrolling-navbar fixed-top text-center">
+        <a class="navbar-brand" href="#"><img src={logo} alt="" width="50"/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"><
+            /span>
         </button>
         <div className="navContent">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
