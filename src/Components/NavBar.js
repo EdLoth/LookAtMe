c256c750ae99e63db00a27e2747f7c0e740e54d9
Eduardo Ramos <eduardosram10@gmail.com>
2021-06-01 17:35:27 -0300

    Update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -10,8 +10,7 @@ export default function NavBar() {
         <a class="navbar-brand" href="#"><img src={logo} alt="" width="50"/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"><
-            /span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="navContent">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
