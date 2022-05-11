195bca2f52b84dc0fc89c93dd88b9c01713e88d7
Eduardo <eduardosram10@gmail.com>
2022-05-11 03:51:08 -0300

    Adicionando Contador na Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from '../Styles/Pages/Home.module.css';
 import Typewriter from "typewriter-effect";
 
 
 import Lottie from 'react-lottie';
 import animationData from '../assets/animation.json';
-import switchLottie from '../assets/toggleTheme.json';
+// import switchLottie from '../assets/toggleTheme.json';
 import NavBar from '../Components/NavBar';
 import { Link } from 'react-router-dom';
 
@@ -20,81 +20,91 @@ export default function Home() {
     }
   };
 
-  const switchLottieOptions = {
-    loop: false,
-    autoplay: false,
-    animationData: switchLottie,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  };
+  // Obtém a data/hora atual
+  const dateObj = new Date();
+  const dayName = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"]
+  const monName = ["janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro"]
 
-  const [isClicked, setClicked] = useState(false);
-  const [animationState, setAnimationState] = useState({
-    isStopped: true, isPaused: false,
-    direction: -1,
-  })
+  console.log("<h1> Hoje é " + dayName[dateObj.getDay() ] + ", " + dateObj.getDate () + " de " + monName[dateObj.getMonth() ]   +  " de "  +     dateObj.getFullYear () + ". </h1>")
+
+  // const switchLottieOptions = {
+  //   loop: false,
+  //   autoplay: false,
+  //   animationData: switchLottie,
+  //   rendererSettings: {
+  //     preserveAspectRatio: 'xMidYMid slice'
+  //   }
+  // };
+
+  // const [isClicked, setClicked] = useState(false);
+  // const [animationState, setAnimationState] = useState({
+  //   isStopped: true, isPaused: false,
+  //   direction: -1,
+  // })
 
 
   return (
-   <div id="fadeIn">
-    <NavBar />
+    <div id="fadeIn">
+      <NavBar />
 
-    <div id={styles.homeContentPage} className="container-fluid">
+      <div id={styles.homeContentPage} className="container-fluid">
 
-      <div id={styles.fadeIn} className="row">
-        <div id={styles.contentHomeResponsive} className="container-fluid">
+        <div id={styles.fadeIn} className="row">
+          <div id={styles.contentHomeResponsive} className="container-fluid">
 
-        <div className="col-6 d-flex align-items-center">
-            <div className={styles.fadeIn}>
-              <Lottie
-                options={defaultOptions}
-                style={{marginLeft:"5vh"}}
-                height={600}
-                width={840}
-              />
+            <div className="col-6 d-flex align-items-center">
+              <div className={styles.fadeIn}>
+                <Lottie
+                  options={defaultOptions}
+                  style={{ marginLeft: "5vh" }}
+                  height={600}
+                  width={840}
+                />
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div id={styles.contentRight} className="container-fluid">
+                <p id={styles.titleHomePage}>Olá,</p>
+                <div className={styles.displayNameHome}>
+                  <p id={styles.subTitleHomePage}>Eu sou </p>
+                  <p id={styles.nameHomePage}>{"<Eduardo Ramos>"}</p>
+                </div>
+                <div className={styles.displayNameHome}>
+                  <p id={styles.descriptionHomePage}>
+                    Desenvolvedor
+                  </p>
+                  <p id={styles.functionHomePage}>
+                    <Typewriter
+                      options={{
+                        autoStart: true,
+                        loop: true,
+                        delay: 50,
+                        strings: [
+                          "React JS.",
+                          "Next JS.",
+                          "React Native.",
+                          "Flutter.",
+                          "Web.",
+                        ]
+                      }}
+                    />
+                  </p>
+                </div>
+                <Link to="/About" id={styles.buttonLeanMore}>Ver Mais</Link>
+              </div>
+
             </div>
           </div>
 
-          <div className="col-6">
-            <div id={styles.contentRight} className="container-fluid">
-              <p id={styles.titleHomePage}>Olá,</p>
-              <div className={styles.displayNameHome}>
-                <p id={styles.subTitleHomePage}>Eu sou </p>
-                <p id={styles.nameHomePage}>{"<Eduardo Ramos>"}</p>
-              </div>
-              <div className={styles.displayNameHome}>
-                <p id={styles.descriptionHomePage}>
-                  Desenvolvedor
-                </p>
-                <p id={styles.functionHomePage}>
-                  <Typewriter
-                    options={{
-                      autoStart: true,
-                      loop: true,
-                      delay: 50,
-                      strings: [
-                        "React JS.",
-                        "Next JS.",
-                        "React Native.",
-                        "Flutter.",
-                        "Web.",
-                      ]
-                    }}
-                  />
-                </p>
-              </div>
-              <Link to="/About" id={styles.buttonLeanMore}>Ver Mais</Link>
-            </div>
-            
-          </div>
+
         </div>
-        
-
-      </div>
-      <div className="row" >
+        <div className="row" >
           <div className="col-12" id={styles.contentSwitchTheme}>
-            <button onClick={() => {
+          <h1 className={styles.dateHome}> 
+          { dayName[dateObj.getDay() ] + ", " + dateObj.getDate ()} {monName[dateObj.getMonth()]}
+          </h1>
+            {/* <button onClick={() => {
               const reverseAnimation = -1;
               const normalAnimation = 1;
 
@@ -118,10 +128,10 @@ export default function Home() {
                   width={120}
                 />
               </div>
-            </button>
+            </button> */}
           </div>
         </div>
+      </div>s
     </div>
-   </div>
   );
 }
